7cf2ea1b555748db035b6ba0fb9de7e332209f73
Nicolly Lima <nicolly.lima@fortics.com.br>
2024-02-14 16:22:54 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var nome = 'André'
+var idade = 28;
+var possuiFaculdade= true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preço = 25;
+var totalComprado = 5;
+var totalPreço = totalComprado * preço;
+
+
+
